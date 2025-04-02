157c1a23a375015a61f18f8de85363fd9a5b034d
aaron victor villariez <aaronvillariez@gmail.com>
2025-04-02 21:22:22 +0800

    Adding some funtion to AddGenderForm and waring to an error imputs

--- client/src/pages/gender/Genders.tsx
+++ client/src/pages/gender/Genders.tsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
+import AlertMessage from "../../components/AlertMessage";
 import AddGenderForm from "../../components/forms/AddGenderForm";
 import GendersTable from "../../components/tables/GendersTable";
 import MainLayout from "../layout/MainLayout";
 
 const Genders = () => {
+  const [message, setMessage] = useState("");
+  const [isSuccess, setisSuccess] = useState(false);
+  const [isVisible, setisVisible] = useState(false);
+
+  const handleShowAlertMessage = (
+    message: string,
+    isSuccess: boolean,
+    isVisible: boolean
+  ) => {
+    setMessage(message);
+    setisSuccess(isSuccess);
+    setisVisible(isVisible);
+  };
+
+  const handleCloseAlterMessage = () => {
+    setMessage("");
+    setisSuccess(false);
+    setisVisible(false);
+  };
+
   const content = (
     <>
+      <AlertMessage
+        message={message}
+        isSuccess={isSuccess}
+        isVisible={isVisible}
+        onClose={handleCloseAlterMessage}
+      />
       <div className="row">
         <div className="col-md-4">
-          <AddGenderForm />
+          <AddGenderForm
+            onGenderAdded={(message) => {
+              handleShowAlertMessage(message, true, true);
+            }}
+          />
         </div>
         <div className="col-md-8">
           <GendersTable />
