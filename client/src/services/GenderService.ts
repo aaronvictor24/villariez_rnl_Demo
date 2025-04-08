ce95501e33842853f6d05db30ec464dd0b6c2d32
aaron victor villariez <aaronvillariez@gmail.com>
2025-04-08 20:55:21 +0800

    Updating deleteGenderForm

--- client/src/services/GenderService.ts
+++ client/src/services/GenderService.ts
@@ -29,5 +29,12 @@ const GenderService = {
         throw error;
       });
   },
+  destroyGender: async (genderId: number) => {
+    return AxiosInstance.put(`/destroyGender/${genderId}`)
+      .then((response) => response)
+      .catch((error) => {
+        throw error;
+      });
+  },
 };
 export default GenderService;
